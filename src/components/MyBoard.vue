06f95e25c62750bb1a2bf778de5420291817155f
yuekin.wang <yuelin.wang@xbongbong.com>
2023-07-24 16:02:11 +0800

    feat: 新增查找功能

--- src/components/MyBoard.vue
+++ src/components/MyBoard.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="board-main">
     <div class="board-body">
+      <div class="find-func">
+        <p class="find-func-text">
+          查询方法：
+        </p>
+        <el-select v-model="findFunc" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </div>
       <div class="find">
-        <p class="find-text">
-          当前查询范围为播放量前
-        </p>
-         <!-- :min="1" :step="1" -->
-        <!-- <el-input class="input no-number" v-model.number="input" oninput="value=value.replace(/[^0-9]/g,'')" type="number"></el-input> -->
-        <el-input class="input no-number" v-model.number="input" :min="1" :max="49901" :step="1"></el-input>
-        <!-- <el-input-number v-model="input" :min="10" :controls="false"></el-input-number> -->
-        <p class="find-out-data">
-          ~{{ +input + 9 }}
-        </p>
+        <!-- uid搜索 -->
+        <div v-if="findFunc === 4">
+          <el-input v-model="findData" placeholder="请输入用户uid"></el-input>
+        </div>
+        <!-- 用户名搜索 -->
+        <div v-if="findFunc === 3">
+          <el-input v-model="findData" placeholder="请输入用户名"></el-input>
+        </div>
+        <!-- 范围搜索 -->
+        <div v-if="findFunc === 2">
+          <p class="find-text">
+            当前查询范围为播放量前
+          </p>
+          <el-input class="input no-number" v-model.number="input" :min="1" :max="49901" :step="1"></el-input>
+          <p class="find-out-data">
+            ~{{ +input + 9 }}
+          </p>
+        </div>
       </div>
       <div class="btn">
         <el-button type="info" plain @click="input -= 10">上一组</el-button>
@@ -32,8 +53,15 @@ export default {
   data () {
     return {
       input: 1,
+      findFunc: 4,
+      options: [
+        { value: 4, label: 'uid搜索' },
+        { value: 3, label: '用户名搜索' },
+        { value: 2, label: '范围搜索' }
+      ],
       allData: [],
-      getData: []
+      getData: [],
+      findData: ''
     }
   },
   watch: {
@@ -48,11 +76,17 @@ export default {
         this.getChart()
         // console.log(newVal)
       }
+    },
+    findData: {
+      handler (newVal, oldVal) {
+        this.debounce(this.getFindRes, 500)
+      },
+      deep: true
     }
   },
   created () {
     this.allData = json50000.data
-    this.getData = this.allData.slice(0, 24)
+    this.getData = this.allData.slice(0, 10)
   },
   mounted () {
     this.getChart()
@@ -122,6 +156,23 @@ export default {
           }
         ]
       })
+    },
+    getFindRes () {
+      this.getData = []
+      // for (let i = 0; i < this.allData.length; i++) {
+      for (let i = 0; i < 10; i++) {
+        if (this.allData[i][this.findFunc] === this.findData) {
+          this.getData.push(this.allData[i])
+          break
+        }
+      }
+      this.getChart()
+    },
+    debounce (fn, wait) {
+      if (this.timer !== null) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(fn, wait)
     }
   }
 
@@ -130,7 +181,7 @@ export default {
 
 <style lang="less" scoped>
 .board-main {
-  width:100%;
+  width: 100%;
   height: 100%;
   p {
     margin: 0;
@@ -144,10 +195,17 @@ export default {
     flex-direction: column;
     justify-content: center;
     font-size: 14px;
+    .find-func {
+      display: flex;
+      justify-content: center;
+      .find-func-text {
+        line-height: 40px;
+      }
+    }
     .find {
       display: flex;
       line-height: 40px;
-      margin: 0 auto;
+      margin: 10px auto;
       .find-text {
         width: 160px;
       }
@@ -160,7 +218,7 @@ export default {
       }
     }
     .btn {
-      margin-top: 10px;
+      // margin-top: 10px;
       display: flex;
       justify-content: center;
     }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import { Button, Input, Link } from 'element-ui'
+import { Button, Input, Link, Select, Option } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Input)
 Vue.use(Link)
+Vue.use(Select)
+Vue.use(Option)
